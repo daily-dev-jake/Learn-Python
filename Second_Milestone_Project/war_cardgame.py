dd16dc459ad9a69eedab26c6ed8723d0cece5399
ITRecursion <jlele92@outlook.com>
2022-05-25 18:12:44 +0800

    Started Data Structure course to construct a linked list

--- Second_Milestone_Project/war_cardgame.py
+++ Second_Milestone_Project/war_cardgame.py
@@ -31,11 +31,41 @@ class Deck:
         return self.all_cards.pop()
         
 #TODO Player class!
+class Player:
+    '''1. Player should be able to add both 'a single card' or 'multiple cards' to their list on hand.
+    2. Player class should allow translating a Deck (hand of cards) with a bottom / top from the stack of a Python List. (Draw card = Deck -1 from top & Hand +1 on top) 
+    3. Player play a card (top card of hand will be played) = cards.pop(0)
+    4. Player add to end of list = cards.append('card1')
+    5. If adding multiple cards to Player hand (to merge 1 list to another), = cards.extend([card1,card2])
+    '''
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+        
+    def remove_one(self):
+        '''Removes first card'''
+        return self.all_cards.pop(0)
+    
+    def add_cards(self,new_cards):
+        ''' Uses diff methods to add cards into player's hand properly'''
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+    
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
 
 new_deck = Deck()
 new_deck.shuffle()
-for i in range(5):
-    print(new_deck.all_cards[i])    
+
+player1 = Player("Jose")
+print(player1)
+player1card = new_deck.deal_one()
+
+player1.add_cards(player1card)
+print(player1card)
+print(player1)
 
 """three_clubs = Card("Clubs","Three")
 print(three_clubs)"""
